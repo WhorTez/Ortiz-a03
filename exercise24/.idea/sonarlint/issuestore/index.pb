45f04ee7067a36f99944b94f92a31fd7c7587bb1
WhorTez <dortiz1@knights.ucf.edu>
2021-09-27 15:59:04 -0400

    pseudocode for exercise25

---
+++ exercise24/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+S
+#src/main/java/baseline/Anagram.java,a\9\a9ab643a19ace79cab96409c9973435658d4b619
+V
+&src/main/java/baseline/Solution24.java,6\a\6a3518ac797c1fdae3479160a6036bf1929dcb92
---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,27 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+public class Solution25 {
+    //Get a password from the user
+        //Prompt for password
+            //Store password
+
+    //---------------------------------------
+
+    //Call function passwordValidator
+        //Function will contain an if statement to determine the strength of the password entered
+            //if password contains only numbers and is fewer than 8 characters
+                //password is very weak
+            //if password contains only letters and is fewer than 8 characters
+                //password is weak
+            //if password contains letters and at least one number and is at least eight characters
+                //password is strong
+            //if password  contains letters, numbers, and special characters and is at least eight characters
+                //password is as good as it can be and is very strong
+
+    //----------------------------------------
+
+    //Output will let user know the strength of the password they entered and recommend an appropriate action
