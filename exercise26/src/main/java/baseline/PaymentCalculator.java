390f033676c2527562cbfb9620c9c221e7bc36d9
WhorTez <dortiz1@knights.ucf.edu>
2021-09-29 14:25:34 -0400

    psuedocode for exercise26

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class PaymentCalculator {
+
+    /*
+    public int calculateMonthsUntilPaidOff(){
+        This method will get values passed from the main class
+
+        Use formula to get months remaining
+            round remaining months up and return it
+     */
+}
---
+++ exercise26/src/main/java/baseline/exercise26.java
@@ -0,0 +1,43 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class exercise26 {
+private static final Scanner in = new Scanner(System.in);
+
+
+    public static void main(String[] args){
+        //Call method calculateMonthsUntilPaidOff using variables obtained from this class
+        //print out the value returned from the above method
+    }
+
+   /*public int getAPR(){
+        this method will get the APR in percentage from the user
+            Ask user for the APR
+                Divide the input by 100 to get the percentage
+            Divide APR by 365 to get daily rate i
+
+         return the APR
+         }
+*/
+
+    /*
+     public int getBalance(){
+       this method will ask the user for the credit card balance
+
+       return the balance
+       }
+       */
+
+    /*
+    public int getMonthlyPayment(){
+        Get the minimum monthly payment the User can make
+
+        return monthly payment
+        }
+     */
+}
