ce24e27202108546579f85cb68cef352783ba1e3
WhorTez <dortiz1@knights.ucf.edu>
2021-10-02 12:46:43 -0400

    exercise29 psuedocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,23 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+public class Solution29 {
+    public static void main(String[] args){
+       //print out the total 
+    }
+
+    public int input(){
+        //Prompt a user to enter rate of return(r)
+        // if user enters any invalid input such as a zero or any non-numeric value don't exit program loop to allow new input
+        //use chapter 11 sample code for assistance on tha
+        //return valid input
+    }
+
+    public int ruleOf72Calc(){
+        //use formula provided and r to calculate years
+        // return total
+    }
+}
