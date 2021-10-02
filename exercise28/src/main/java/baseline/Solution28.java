da1542ebe83ec95089b6fb750e786c1eacdcfe22
WhorTez <dortiz1@knights.ucf.edu>
2021-10-02 12:27:55 -0400

    completed exercise28

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -4,16 +4,30 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
     private int total;
+    private static final Scanner in = new Scanner(System.in);
 
-    public int addNumbers(){
+    public int input() {
+        System.out.print("Enter a number: ");
+        return in.nextInt();
+    }
+    public int addNumbers(int num){
         //prompt user to enter a number 5 times using a loop
         //each time a number is entered add it to the sum of all numbers
+        total += num;
         //return the sum
+        return total;
     }
     public static void main(String[] args){
+        Solution28 s = new Solution28();
         //set total equal to the return value of method addNumbers
+        for(int i =0; i<5; i++){
+            s.addNumbers(s.input());
+        }
         //print out the sum of all numbers
+        System.out.println("The sum is "+ s.total);
     }
 }
--- exercise28/src/test/java/baseline/Solution28Test.java
+++ exercise28/src/test/java/baseline/Solution28Test.java
@@ -1,13 +1,16 @@
 package baseline;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution28Test {
 Solution28 s = new Solution28();
-    @Test
-    void addNumbers() {
-        assertEquals(15, s.addNumbers());
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4,5})
+    void addNumbers(int num) {
+
+        assertEquals(15, s.addNumbers(int num));
     }
 }
