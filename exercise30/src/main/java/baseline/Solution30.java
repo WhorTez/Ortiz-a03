78f7bcfaaadc08eb491fc0ece3db9ed1d1341b34
WhorTez <dortiz1@knights.ucf.edu>
2021-10-03 12:08:02 -0400

    exercise30 pseudocode

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,22 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args){
+        //calls the print times table method
+    }
+
+    public void printTimesTable(){
+        //use two nested loops each printing out 1-12 inclusive
+        //calls the multiplication method with the loop variables as input
+        //print out results each time
+        //use %-10.3 to create columns
+    }
+
+    public int multiplication(int a, int b){
+        //returns a * b
+    }
+}
