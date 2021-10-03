4a264bad557ae6d562b2d83d7e95174a092b05b0
WhorTez <dortiz1@knights.ucf.edu>
2021-10-03 12:37:28 -0400

    exercise31 psuedocode

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,35 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+public class Solution31 {
+    public static void main(String[] args){
+        //get age and resting heart rate from the user
+        //print said age and heart rate
+        //call a function that will print a table displaying the info
+    }
+
+    public int getAge(){
+    //prompt the user for their age
+    //return the input
+}
+
+    public int getRestingHeartRate() {
+        //prompt for the resting heart rate
+        // return the input
+    }
+
+    public int karvonenHeartRateCalc(int age, int RHR){
+        //return the heart rate using the provided formula
+    }
+
+    public void printTable(){
+        //Use a loop starting at 55 and increases by 5
+            //print out intensity followed by a dashed line
+            //print out the karvonen heart rate
+            //print out %n
+    }
+
+}
