0027ff6083328479a996984483decb97495d54c2
WhorTez <dortiz1@knights.ucf.edu>
2021-10-03 22:57:23 -0400

    exercise34 psuedocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,33 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution34 {
+    private static final Scanner in = new Scanner(System.in);
+    private String removed;
+    private final String[] employees = {"Bruce Banner", "Wonder Woman", "Tony Stark", "Dr. Strange", "Your Mom"};
+
+    public static void main(String[] args){
+        //print the first array with 5 employees
+        //ask which name to be removed
+        //call the first array but with new arrays parameters
+    }
+
+    private String nameToRemove(){
+        //ask user which name they want to remove
+        // return input
+    }
+
+    public void initialArray(String[] names){
+        //print how many people are employed
+        // use a for loop to print each employees name
+    }
+
+    public String[] removeFromInitialArray(String removed, String[] names){
+        //make a copy of the first array with one less entry
+    }
+}
