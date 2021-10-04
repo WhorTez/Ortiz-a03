c5c6206bfb26f457829f56a37d3e8f8966b527aa
WhorTez <dortiz1@knights.ucf.edu>
2021-10-03 22:26:17 -0400

    exercise33 psuedocode

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,34 @@
+/*
+ *   UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *   Copyright 2021 Duane Ortiz
+ */
+package baseline;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution33 {
+    private static final Scanner in = new Scanner(System.in);
+    private static final Random r = new Random();
+    public final String[] answers = {"Yes", "NO", "Maybe", "Ask again later."};
+
+    public static void main(String[] args){
+        //prompt user for a simple question
+        //get a random number from an RNG
+        //print out one of the answers listed above
+    }
+
+    public void askQuestion(){
+        //ask for a question
+        //return the question
+    }
+
+    public int RNG(){
+        //return a random number 1-4
+    }
+
+    public String giveAnswer(){
+        //returns an answer as the string
+    }
+
+}
